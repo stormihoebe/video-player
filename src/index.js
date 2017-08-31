dd41924abc9e32f3493a1e009e094446c68f4985
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-08-31 15:17:35 -0700

    add youtube search api and simple input field in searchBar component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/Search_bar';
 
 const API_KEY = 'AIzaSyDQFcpcvhIVvda3fBVKP2hubuyJJX52fiY';
 const App = () => {
-  return <div>Hello!</div>;
+  return (<div>
+
+    <SearchBar />
+  </div>);
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
