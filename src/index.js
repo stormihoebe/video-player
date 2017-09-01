d0e22ed849e06fbabc84940ae7e2489a8b06457e
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-09-01 11:41:25 -0700

    add props and elements to video list and video list item components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/Search_bar';
-
+import VideoList from './components/Video_list';
 const API_KEY = 'AIzaSyDQFcpcvhIVvda3fBVKP2hubuyJJX52fiY';
-const App = () => {
-  return (
-    <div>
 
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = { videos: []};
+    YTSearch({key: API_KEY, term: 'kittens'}, (videos)=>{
+      this.setState({videos})
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos}/>
+      </div>
+    );
+
+  }
+
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
